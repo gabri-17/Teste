2eb9ff8e254bc04c1ed9ce93868572e95ea67fc6
gabri-17 <ga170120@gmail.com>
2023-06-01 01:06:05 -0300

    Adicionar divisão

--- teste.py
+++ teste.py
@@ -1,8 +1,10 @@
-from func import soma, mult
+from func import soma, mult, div
 def test_soma():
     assert soma([1, 2, 3]) == 6, "Deve ser 6"
 def test_mult():
     assert mult((2, 3, 4)) == 24, "Deve ser 24"
+def test_div():
+    assert div((2, 3, 4)) == 24, "Deve ser 24"
 if __name__ == "__main__":
     test_soma()
     test_mult()
